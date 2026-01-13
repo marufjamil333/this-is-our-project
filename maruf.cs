abd5039a1e98a2b2037a76125b8f36b20d0a8bfc
MARUF JAMIL <128314952+marufjamil333@users.noreply.github.com>
2026-01-13 20:47:40 +0600

    Create maruf.cs

---
+++ maruf.cs
@@ -0,0 +1,12 @@
+string name = "Rahim";
+int age = 22;
+bool isStudent = true;
+
+int a = 10;
+int b = a;
+Console.WriteLine(b);
+
+Console.WriteLine(name);
+Console.WriteLine(age);
+Console.WriteLine(isStudent);
+Console.WriteLine(a);
